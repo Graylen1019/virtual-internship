86f3ac516399baad61f86efe6049fb532927c046
Graylen <delilhabigelow@gmail.com>
2025-08-14 23:21:47 -0400

    forgot to make the login button clickable on fall back component xD.

--- src/app/modules/components/dashboard/auth.tsx
+++ src/app/modules/components/dashboard/auth.tsx
@@ -1,29 +1,18 @@
 import { useModal } from "@/app/context/modal-context";
 import Image from "next/image";
-import { useState } from "react";
+
 
 export const NoUserContent = () => {
-    const { isSignInOpen, closeSignInModal, openSignInModal } = useModal();
-    const [isSignUpOpen, setIsSignUpOpen] = useState(false);
+    const { openSignInModal } = useModal();
 
-    const openSignUpModal = () => {
-        setIsSignUpOpen(true);
-        closeSignInModal();
-    };
 
-    const closeSignUpModal = () => setIsSignUpOpen(false);
-
-    const handleSignInFromSignUp = () => {
-        closeSignUpModal();
-        openSignInModal();
-    };
     return (
         <div className="w-full py-10 px-6 mx-auto max-w-[1070px]">
             <div className="max-w-[460px] flex flex-col mx-auto items-center">
-                <Image height={712} width={1033} src={ `/assets/login.webp`} alt="login-image" />
+                <Image height={712} width={1033} src={`/assets/login.webp`} alt="login-image" />
                 <h1 className="text-2xl font-bold text-[#032b41] text-center mb-4">Log in to your account to see your library.</h1>
-                <button className="bg-[#2bd97c] text-[#032b41] w-[180px] h-10 rounded-sm transition-colors duration-200 flex items-center justify-center min-w-[180px] hover:bg-[#20ba68]">Login</button>
+                <button onClick={openSignInModal} className="bg-[#2bd97c] text-[#032b41] w-[180px] h-10 rounded-sm transition-colors duration-200 flex items-center justify-center min-w-[180px] hover:bg-[#20ba68]">Login</button>
             </div>
         </div>
-    );
+    )
 }
